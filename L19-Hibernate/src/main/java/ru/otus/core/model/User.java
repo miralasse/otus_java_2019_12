e6522ba07e1962902822e4bb9c196838dc774770
Evgeniya_Yanchenko <evgeniya.yanchenko@cinimex.ru>
2020-06-12 10:35:33 +0300

    init commit

---
+++ L19-Hibernate/src/main/java/ru/otus/core/model/User.java
@@ -0,0 +1,30 @@
+package ru.otus.core.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+}
