875981a6400af29d3889c86f6035c91767d647ca
Evgeniya_Yanchenko <evgeniya.yanchenko@cinimex.ru>
2020-07-08 13:43:18 +0300

    fixes after review

--- L23-WebServer/src/main/java/ru/otus/servlet/AuthorizationFilter.java
+++ L23-WebServer/src/main/java/ru/otus/servlet/AuthorizationFilter.java
@@ -1,12 +1,23 @@
 package ru.otus.servlet;
 
-import javax.servlet.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class AuthorizationFilter implements Filter {
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthorizationFilter.class);
 
     private ServletContext context;
 
@@ -23,7 +34,7 @@ public class AuthorizationFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
 
         String uri = request.getRequestURI();
-        this.context.log("Requested Resource:" + uri);
+        logger.info("Requested Resource:" + uri);
 
         HttpSession session = request.getSession(false);
 
